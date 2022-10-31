7be9f1b28decc7ca55879503eaad77b0bce3d46f
MaroulisManolis <manmaroulis@gmail.com>
2022-10-31 07:56:58 +0200

    creating a more dynamic function for challnges folder in the views.py file

--- challenges/views.py
+++ challenges/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+monthly_challenges = {
+    "january": "Learn Django for at least 20 minutes per day!",
+    "february": "Walk for at least 30 minutes per day!",
+    "march": "Tend to your garden for at least 40 minutes per day!",
+    "april": "Have a dinner with friends at least once per week!",
+    "may": "Buy birthday presents for all your friends!",
+    "june": "Update your CV and look after for a descent job!",
+    "july": "Go to the sea for at least 4 days per week!",
+    "august": "Organise your summer vacation!",
+    "september": "Plant your winter time plants!",
+    "october": "Start exercising for at least 30 minutes per day!",
+    "november": "Organise your vacation for abroad",
+    "december": "Pass Christmas time with all family members!"
+}
+
 
 # Create your views here.
 
+def monthly_challenges_by_number(request, month):
+    return HttpResponse(month)
+
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Learn Django for at least 20 minutes per day!"
-    elif month == "february":
-        challenge_text = "Walk for at least 30 minutes per day!"
-    elif month == "march":
-        challenge_text = "Tend to your garden for at least 40 minutes per day!"
-    else:
-        return HttpResponseNotFound("This month is not supported")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
